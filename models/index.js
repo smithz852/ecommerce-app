f76871aa24fc7119451dbdcfbc16ba4f34b0ef43
Zach Smith <zachsmith852@gmail.com>
2024-02-01 15:52:07 -0800

    Fix bug with products POST route

--- models/index.js
+++ models/index.js
@@ -18,13 +18,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id',
+  foreignKey: 'product_id',
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'product_id',
+  foreignKey: 'tag_id',
   onDelete: 'CASCADE',
 })
 
